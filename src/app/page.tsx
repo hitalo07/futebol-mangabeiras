07b82b0cd9ae5177370e8ec7f8626b457436e58e
hitalo <hsousa@bestprojects.com.br>
2023-12-04 18:29:36 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,95 @@
-import Image from 'next/image'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="text-gray-900 h-screen bg-gray-900">
+      <Header />
+      <div className="py-8">
+        <div className="max-w-screen-xl px-2 mx-auto">
+          <table className="w-full text-base">
+            <thead>
+              <tr className="border-b border-gray-600">
+                <th className="text-left p-1 text-gray-50 pb-2 text-gray-100">&nbsp;</th>
+                <th className="text-left p-1 text-gray-50 pb-2 text-gray-100"><abbr title="Clibes">Clube</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2"><abbr title="Pontos">Pts</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2"><abbr title="Partidas Jogadas">Pj</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2"><abbr title="Vitórias">VIT</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2"><abbr title="Empates">E</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2"><abbr title="Derrotas">DER</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2 hidden lg:table-cell"><abbr title="Gols a favor">GM</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2 hidden lg:table-cell"><abbr title="Gols contra">GC</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2"><abbr title="Saldo de gols">SG</abbr></th>
+                <th className="text-center text-gray-100 p-1 pb-2 hidden md:table-cell"><abbr title="Gols em casa e fora de casa">Últimos 5</abbr></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-green-100 bg-opacity-30">
+                <td className="text-left p-1 text-gray-50">1</td>
+                <td className="text-left p-1 text-gray-50"><span className="hidden md:inline">Central</span><span className="md:hidden">Central</span></td>
+                <td className="text-center text-gray-50 p-1">86</td>
+                <td className="text-center text-gray-50 p-1">38</td>
+                <td className="text-center text-gray-50 p-1">27</td>
+                <td className="text-center text-gray-50 p-1">5</td>
+                <td className="text-center text-gray-50 p-1">6</td>
+                <td className="text-center text-gray-50 p-1 hidden lg:table-cell">83</td>
+                <td className="text-center text-gray-50 p-1 hidden lg:table-cell">32</td>
+                <td className="text-center text-gray-50 p-1">51</td>
+                <td className="text-center text-gray-50 p-1 hidden md:table-cell">
+                  <div className="flex items-center justify-center w-full">
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-red-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-red-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="text-left p-1 text-gray-50">2</td>
+                <td className="text-left p-1 text-gray-50"><span className="hidden md:inline">Vila Cardoso</span><span className="md:hidden">Vila C.</span></td>
+                <td className="text-center text-gray-50 p-1">74</td>
+                <td className="text-center text-gray-50 p-1">38</td>
+                <td className="text-center text-gray-50 p-1">21</td>
+                <td className="text-center text-gray-50 p-1">11</td>
+                <td className="text-center text-gray-50 p-1">6</td>
+                <td className="text-center text-gray-50 p-1 hidden lg:table-cell">73</td>
+                <td className="text-center text-gray-50 p-1 hidden lg:table-cell">44</td>
+                <td className="text-center text-gray-50 p-1">29</td>
+                <td className="text-center text-gray-50 p-1 hidden md:table-cell">
+                  <div className="flex items-center justify-center w-full">
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-red-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-red-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="text-left p-1 text-gray-50">3</td>
+                <td className="text-left p-1 text-gray-50"><span className="hidden md:inline">Morro do Chupé</span><span className="md:hidden">Morro C.</span></td>
+                <td className="text-center text-gray-50 p-1">69</td>
+                <td className="text-center text-gray-50 p-1">38</td>
+                <td className="text-center text-gray-50 p-1">20</td>
+                <td className="text-center text-gray-50 p-1">9</td>
+                <td className="text-center text-gray-50 p-1">9</td>
+                <td className="text-center text-gray-50 p-1 hidden lg:table-cell">68</td>
+                <td className="text-center text-gray-50 p-1 hidden lg:table-cell">42</td>
+                <td className="text-center text-gray-50 p-1">26</td>
+                <td className="text-center text-gray-50 p-1 hidden md:table-cell">
+                  <div className="flex items-center justify-center w-full">
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                    <div className="bg-green-500 h-4 w-4 mx-0.5 rounded-full"></div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header () {
+  return (
+    <nav className="flex gap-4 bg-green-600 w-full p-4 justify-center items-center">
+      <a href="#" className="text-white text-md">
+        Partidas
+      </a>
+      <a href="#" className="text-white text-md">
+        Classificação
+      </a>
+    </nav>
+  )
+}
